3f8a85ea81be51a6c231550431d96db1b2e0114e
Suzic-MAC2019W <suzicletter@msn.com>
2019-10-09 09:33:27 +0800

    代码格式化

--- virtuallab/virtuallab/Environment.aspx
+++ virtuallab/virtuallab/Environment.aspx
@@ -1,10 +1,9 @@
 ﻿<%@ Page Title="进行实验" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Environment.aspx.cs" Inherits="virtuallab.Environment" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h4>&nbsp;</h4>
-    <div class="row">
+    <div class="row" style="position:relative; top:20px; height:50px;">
         <div class="col-md-2">
-            <asp:Button runat="server" OnClick="Func" Text="重新加载实验模板" CssClass="btn btn-default form-control" />
+            <asp:Button runat="server" OnClick="Func" Text="重新加载模板代码" CssClass="btn btn-default form-control" />
         </div>
         <div class="col-md-6">
         </div>
@@ -20,17 +19,52 @@
     </div>
     <hr />
     <div class="row">
-        <div class="col-md-8">
-            <textarea id="TextArea1" class="col-md-12" rows="25" spellcheck="false"></textarea>
+        <div class="col-md-8" style="height:800px; padding-right:0px; border-style:solid; border-width:thin;">
+            <textarea id="code_text" class="form-control">
+       protected void Page_Init(object sender, EventArgs e)
+        {
+            HtmlGenericControl CodeMirrorJS = new HtmlGenericControl("script");
+            CodeMirrorJS.Attributes.Add("type", "text/javascript");
+            CodeMirrorJS.Attributes.Add("src", ResolveUrl(Page.ResolveClientUrl("~/CM/lib/codemirror.js")));
+            HtmlGenericControl CodeFormatClike = new HtmlGenericControl("script");
+            CodeMirrorJS.Attributes.Add("type", "text/javascript");
+            CodeMirrorJS.Attributes.Add("src", ResolveUrl(Page.ResolveClientUrl("~/CM/mode/clike/clike.js")));
+            HtmlGenericControl CodeMirrorCSS = new HtmlGenericControl("link");
+            CodeMirrorCSS.Attributes.Add("rel", "stylesheet");
+            CodeMirrorCSS.Attributes.Add("href", ResolveUrl(Page.ResolveClientUrl("~/CM/lib/codemirror.css")));
+
+            this.Page.Header.Controls.AddAt(1, CodeMirrorJS);
+            this.Page.Header.Controls.AddAt(1, CodeFormatClike);
+            this.Page.Header.Controls.AddAt(1, CodeMirrorCSS);
+
+            //myJs = new HtmlGenericControl();
+            //myJs.TagName = "script";
+            //myJs.Attributes.Add("type", "text/javascript");
+            //myJs.Attributes.Add("src", ResolveUrl(Page.ResolveClientUrl("~/Content/javascript/javascript.js")));
+            //this.Page.Header.Controls.AddAt(1, myJs);
+
+            //string script = "<script>var editor = CodeMirror.fromTextArea(document.getElementById(\"code\"), {mode: \"text/groovy\",  lineNumbers: true,theme: \"dracula\",lineWrapping: true,matchBrackets: true}); editor.setSize('800px', '950px');</script>";
+            //Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "text", script);
+
+            CurrentLoginUser = SiteMaster.CurrentLoginUser;
+            if (CurrentLoginUser == null)
+                Response.Redirect("~/");
+            else if (CurrentLoginUser.type == 0)
+                Response.Redirect("~/ManagerPage");
+        }
+            </textarea>
         </div>
-        <div class="col-md-4">
-            <textarea id="TextArea2" class="col-md-12" rows="25" contenteditable="false" spellcheck="false"></textarea>
+        <div class="col-md-4" style="height:800px; padding-left:5px;">
+            <textarea id="debug_text" class="form-control">
+Compile results >>
+________________________________________________
+            </textarea>
         </div>
     </div>
-    <h4>&nbsp;</h4>
-    <div class="row">
+    <div class="row" style="position:relative; top:20px; height:50px;">
         <div class="col-md-2">
             <asp:Button runat="server" OnClick="Func" Text="查看板卡效果" CssClass="btn btn-default form-control" />
         </div>
     </div>
+
 </asp:Content>
--- virtuallab/virtuallab/Environment.aspx.cs
+++ virtuallab/virtuallab/Environment.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using virtuallab.Models;
 
@@ -14,6 +15,8 @@ namespace virtuallab
 
         protected void Page_Init(object sender, EventArgs e)
         {
+            InitCodeMirrorStyles();
+
             CurrentLoginUser = SiteMaster.CurrentLoginUser;
             if (CurrentLoginUser == null)
                 Response.Redirect("~/");
@@ -29,6 +32,64 @@ namespace virtuallab
         protected void Func(object sender, EventArgs e)
         {
 
+        }
+
+        protected void InitCodeMirrorStyles()
+        {
+            HtmlGenericControl CodeMirrorJS = new HtmlGenericControl("script");
+            CodeMirrorJS.Attributes.Add("src", ResolveUrl(Page.ResolveClientUrl("~/CM/lib/codemirror.js")));
+
+            HtmlGenericControl CodeMirrorCSS = new HtmlGenericControl("link");
+            CodeMirrorCSS.Attributes.Add("href", ResolveUrl(Page.ResolveClientUrl("~/CM/lib/codemirror.css")));
+            CodeMirrorCSS.Attributes.Add("rel", "stylesheet");
+
+            HtmlGenericControl CodeMirrorTheme1 = new HtmlGenericControl("link");
+            CodeMirrorTheme1.Attributes.Add("href", ResolveUrl(Page.ResolveClientUrl("~/CM/theme/zenburn.css")));
+            CodeMirrorTheme1.Attributes.Add("rel", "stylesheet");
+            HtmlGenericControl CodeMirrorTheme2 = new HtmlGenericControl("link");
+            CodeMirrorTheme2.Attributes.Add("href", ResolveUrl(Page.ResolveClientUrl("~/CM/theme/mdn-like.css")));
+            CodeMirrorTheme2.Attributes.Add("rel", "stylesheet");
+
+            HtmlGenericControl CodeScrollbar = new HtmlGenericControl("script");
+            CodeScrollbar.Attributes.Add("src", ResolveUrl(Page.ResolveClientUrl("~/CM/addon/scroll/simplescrollbars.js")));
+
+            HtmlGenericControl CodeFormatClike = new HtmlGenericControl("script");
+            CodeFormatClike.Attributes.Add("src", ResolveUrl(Page.ResolveClientUrl("~/CM/mode/clike/clike.js")));
+            HtmlGenericControl CodeFormatText = new HtmlGenericControl("script");
+            CodeFormatText.Attributes.Add("src", ResolveUrl(Page.ResolveClientUrl("~/CM/mode/textile/textile.js")));
+
+            this.Page.Header.Controls.Add(CodeMirrorCSS);
+            this.Page.Header.Controls.Add(CodeMirrorJS);
+            this.Page.Header.Controls.Add(CodeMirrorTheme1);
+            this.Page.Header.Controls.Add(CodeMirrorTheme2);
+            this.Page.Header.Controls.Add(CodeFormatClike);
+            this.Page.Header.Controls.Add(CodeFormatText);
+            this.Page.Header.Controls.Add(CodeScrollbar);
+
+            string script = "<script>" +
+                "window.onload = function() {" +
+                "   var editor = CodeMirror.fromTextArea(document.getElementById('code_text')," +
+                "   {" +
+                "       mode:'text/x-c++src'," +
+                "       lineNumbers:true," +
+                "       theme:'mdn-like'," +
+                "       matchBrackets:true," +
+                "       identUnit:4," +
+                "       smartIdent:true," +
+                "       indentWithTabs:true," +
+                "   });" +
+                "   editor.setSize('100%', '100%');" +
+                "   var outer = CodeMirror.fromTextArea(document.getElementById('debug_text')," +
+                "   {" +
+                "       mode:'textile'," +
+                "       theme:'zenburn'," +
+                "       identUnit:4," +
+                "       readOnly:true" +
+                "   });" +
+                "   outer.setSize('100%', '100%');" +
+                "};" +
+                "</script>";
+            Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "text", script);
         }
     }
 }
