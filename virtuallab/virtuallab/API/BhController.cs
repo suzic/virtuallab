e8f97ded512b43487a45c08298dca68a73b51a2e
yanhaotbg <yanhaotbg@126.com>
2020-06-05 16:57:16 +0800

    runResultTick 接口修改

--- virtuallab/virtuallab/API/BhController.cs
+++ virtuallab/virtuallab/API/BhController.cs
@@ -107,11 +107,10 @@ namespace virtuallab.API
             RunResultTickRes res= service.runResultTick(req);
             ControllerRunResultTickRes R = new ControllerRunResultTickRes();
             R.fail = res.fail;
-            if (res.fail == 0&&res.effect!=null&&res.effect.Count>11) {
-                R.effect = string.Format("{0}{1}{2}{3}{4}{5}{6}{7},{8},{9},{10},{11}",
+            if (res.fail == 0&&res.effect!=null&&res.effect.Count>4) {
+                R.effect = string.Format("{0},{1},{2},{3},{4}",
                     res.effect[0], res.effect[1], res.effect[2], res.effect[3],
-                    res.effect[4], res.effect[5], res.effect[6], res.effect[7],
-                    res.effect[8], res.effect[9], res.effect[10], res.effect[11]);
+                    res.effect[4]);
             
             }
             return R;
